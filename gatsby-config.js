d191b6071fba885e192f8ebc67f4a6959418e282
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-10-15 10:01:09 -0700

    Added Nunito Sans Google font.

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,6 +81,15 @@ module.exports = {
         icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Nunito Sans\:400,400i,700`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
